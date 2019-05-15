444a7f2b585b1a608c824c8032c4d0120f572c03
Sirin Chankao <sirin.cha@student.mahidol.edu>
2019-05-16 00:49:27 +0700

    Input static variables for Tiger

--- src/main/java/io/muzoo/ooc/ecosystems/Tiger.java
+++ src/main/java/io/muzoo/ooc/ecosystems/Tiger.java
@@ -1,4 +1,18 @@
 package io.muzoo.ooc.ecosystems;
 
 public class Tiger {
+
+    // The age at which a rabbit can start to breed.
+    private static final int BREEDING_AGE = 15;
+    // The age to which a rabbit can live.
+    private static final int MAX_AGE = 200;
+    // The likelihood of a rabbit breeding.
+    private static final double BREEDING_PROBABILITY = 0.05;
+    // The maximum number of births.
+    private static final int MAX_LITTER_SIZE = 2;
+    // The food value of a single animal, other than tiger. In effect, this is the
+    // number of steps a tiger can go before it has to eat again.
+    private static final int PREY_FOOD_VALUE= 3;
+
+
 }
