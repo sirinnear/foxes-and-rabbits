a6c85d4c146b1dd8e4d7d5b2deb4906c1d568512
Sirin Chankao <sirin.cha@student.mahidol.edu>
2019-05-17 01:01:02 +0700

    Setup Hunter class

--- src/main/java/io/muzoo/ooc/ecosystems/Hunter.java
+++ src/main/java/io/muzoo/ooc/ecosystems/Hunter.java
@@ -1,4 +1,29 @@
 package io.muzoo.ooc.ecosystems;
 
-public class Hunter {
+import java.util.List;
+
+public class Hunter extends Actor{
+
+    // A hunter's position
+    private Location location;
+
+
+    public Hunter(int row, int col){
+        setLocation(row, col);
+    }
+
+    @Override
+    public void act(Field currentField, Field updatedField, List newActor) {
+
+    }
+
+    public void setLocation(int row, int col) {
+        this.location = new Location(row, col);
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+
 }
